1968b2058a42d652e1db4ca7fd717ffb81eb2754
Gene Kobilansky <genek149@gmail.com>
2016-08-22 19:31:59 +0000

    added multer and got file size

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var multer  = require('multer');
+var upload = multer({ dest: './uploads/' })
+
 var app = express();
 
 app.use(express.static('public'));
@@ -6,6 +9,15 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + 'public/index.html');
 });
+
+app.post('/file-size', upload.single('userfile'), function (req, res) {
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+  var size = req.file.size;
+  console.log(req.file.size);
+  
+  res.end('Your file is '+ size +' bytes');
+})
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('File metadata app listening on port 8080!');
